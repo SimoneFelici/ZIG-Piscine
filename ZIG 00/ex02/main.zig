04c3b6bf35373e5d556dba8f50d9712d92e39321
SimoneFelici <felicisimone9@gmail.com>
2026-04-29 15:49:25 +0200

    ex02

--- ZIG 00/ex02/main.zig
+++ ZIG 00/ex02/main.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 
 fn print_alphabet() void {
-    var letter: u8 = 'a';
-    while (letter <= 'z') : (letter += 1) {
+    var letter: u8 = 'z';
+    while (letter >= 'a') : (letter -= 1) {
         _ = std.os.linux.write(1, @ptrCast(&letter), 1);
     }
 }
